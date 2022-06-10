497df35692118e55551e3e1f2ee37f572b1236c7
Sir Sanctified <63302923+SirSanctified@users.noreply.github.com>
2022-06-10 09:33:54 +0200

    Delete DateAndTime.java

--- DateAndTime.java
+++
@@ -1,9 +0,0 @@
-
-import java.time.LocalDateTime; // import the LocalDateTime class
-
-public class DateAndTime {
-  public static void main(String[] args) {
-    LocalDateTime myObj = LocalDateTime.now();
-    System.out.println(myObj);
-  }
-}
